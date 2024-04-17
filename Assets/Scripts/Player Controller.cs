fbe852133b9c99948fc2da38d767b78b93608f7f
NebulaPhoenix1 <luke.robb210@gmail.com>
2024-04-17 17:19:48 +0100

    Rewriting Player Movement from Scratch
    Was having issues making movement before so restarted and found some guides: its going much better
    - Added left right movement
    - Jumping and double jumping
    - Dashing (on left shift for now, can be changed later)
    - Camera smoothing is broken due to new movement script, I am planning on fixing soon

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -5,116 +5,108 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/* Need to add:
- * - Double jump counter
- * - Wall climbing
- */
-
+//Movement using old input system
 public class NewBehaviourScript : MonoBehaviour
 {
-    float horizontalInput;
-    //Initalise speed values and rigid body component 
-    //Serialize field just makes values appear to editor
-    [Header("Moving Speeds")]
-    [SerializeField] private float speed = 5;
-    [SerializeField] private float jumpSpeed = 5;
-    [SerializeField] private float wallJumpSpeed = 5;
-    [Header("Jumping Values")]
-    [SerializeField] private float wallJumpCD = 0.2f;
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private LayerMask wallMask;
-    [SerializeField] private float coyoteTime;
+    private float horizontalInput;
+    private float speed = 8f;
+    private float jumpingSpeed = 16f;
+    private bool isFacingRight = true;
 
-    private float currentWallJumpCD;
-    private float coyoteTimeTimer;
+    //Double jump
     private bool doubleJump;
-    Rigidbody2D rb;
-    BoxCollider2D boxColl2D;
-    
 
-    private void Start()
+
+    //Dashing
+    private bool dashAllowed = true;
+    private bool isDashing;
+    private float dashSpeed = 24f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 1.5f;
+
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
+
+    void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
-        boxColl2D = GetComponent<BoxCollider2D>();
-    }
-    private void Update()
-    {
-        //Flip the player by inverting X scale
-        horizontalInput = Input.GetAxis("Horizontal");
-        if(horizontalInput > 0.01f )
+        //Prevents player inputting extra actions while dashing
+        if(isDashing)
         {
-            transform.localScale = Vector3.one;
-        }
-        else if(horizontalInput < -0.01f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
+            return;
         }
 
-        //Grabs horizontal input and moves accordingly, does not change Y axis movement
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-    
-        //If is grounded, reset coyote time timer
-        if(isGrounded())
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        //Jumping
+        if(isGrounded() && !Input.GetButton("Jump"))
         {
-            coyoteTimeTimer = coyoteTime;
-            //doubleJump = true;
-        }
-        //If not grounded, tick down coyote time timer
-        else
-        {
-            coyoteTimeTimer -= Time.deltaTime;
+            doubleJump = false;
         }
 
-        //Jumping Logic
-        //If grounded and not pressing jump, set double jump to false
-        if (isOnWall() == false)
+        if(Input.GetButtonDown("Jump") && (isGrounded() || doubleJump))
         {
-            if (coyoteTimeTimer > 0 && !Input.GetButton("Jump"))
-            {
-                doubleJump = false;
-            }
-            if (Input.GetButtonDown("Jump"))
-            {
-                //If jump pressed, is grounded or double jump is true, make player jump and switch double jump boolean 
-                if (coyoteTimeTimer > 0 || doubleJump)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-                    doubleJump = !doubleJump;
-                }
-            }
+            rb.velocity = new Vector2(rb.velocity.x, jumpingSpeed);
 
+            doubleJump = !doubleJump;
         }
-        //Wall Jump Logic
-        else
-        {
-            if(currentWallJumpCD == 0)
-            {
-                if (Input.GetButtonDown("Jump"))
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, wallJumpSpeed);
-                    currentWallJumpCD = wallJumpCD;
-                }
-            }
-            else
-            {
-                currentWallJumpCD -= Time.deltaTime;
-            }
-        }
-    }
         
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0.0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        //Trigger dash
+        //On left shift for now, will change to double tap later :)
+        if(Input.GetKeyDown(KeyCode.LeftShift) && dashAllowed)
+        {
+            StartCoroutine(dash());
+        }
+        flipPlayer();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Prevents player inputting extra actions while dashing
+        if (isDashing)
+        {
+            return;
+        }
+
+        //Takes horizontal input from update and sets speed 
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+    }
+
     private bool isGrounded()
     {
-        float extraSize = 0.2f;
-        RaycastHit2D boxCheck = Physics2D.BoxCast(boxColl2D.bounds.center, boxColl2D.bounds.size, 0f, Vector2.down, extraSize, groundMask);
-        //Debug.Log(boxCheck.collider);
-        return boxCheck.collider != null;
+        //Used a overlap circle on the groundCheck game object to see if we are grounded
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, ground);
     }
 
-    //Checks the player is on a wall by doing a box cast in front of the player. If it hits an object with the tag 'wall', we must be walled so return true
-    private bool isOnWall()
+    private void flipPlayer()
     {
-        RaycastHit2D boxCheckWall = Physics2D.BoxCast(boxColl2D.bounds.center, boxColl2D.bounds.size, 0f, new Vector2(transform.localScale.x, 0), 0.2f, wallMask);
-        return boxCheckWall.collider != null;
+        //Based on horizontal input, switch isFacingRight
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f) 
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
     }
 
+    private IEnumerator dash()
+    {
+        dashAllowed = false;
+        isDashing = true;
+        float startingGravity = rb.gravityScale;
+        rb.gravityScale = 0; //Sets gravity to 0 so the player doesnt fall during the dash
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0f ); //transform.localScale.x is player direction
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = startingGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        dashAllowed = true;
+    }
 }
