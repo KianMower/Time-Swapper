9c821b4e1be5163dc006228d47218c591dec6d4b
KianMower <kianmower@gmail.com>
2024-04-17 16:09:12 +0100

    Change in time

--- Assets/Scripts/ChangeTime.cs
+++ Assets/Scripts/ChangeTime.cs
@@ -1,36 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChangeTime : MonoBehaviour
 {
     public GameObject present;
-    public GameObject future;
-
-    static bool changeTime = false;    
+    public GameObject future;    
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            changeTime = !changeTime;
-            TimesChanging();
-        }
-
-        
-    }
-
-    void TimesChanging()
-    {
-        if (changeTime)
+        if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             future.SetActive(true);
             present.SetActive(false);
         }
-        else
+
+        if (Input.GetKeyDown(KeyCode.Mouse2))
         {
             future.SetActive(false);
             present.SetActive(true);
         }
-    }
+    }    
 }
