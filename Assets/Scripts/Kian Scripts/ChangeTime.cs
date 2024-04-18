673fbb3b7c9980152cf4d79cf2e8175e1e8a03ed
KianMower <kianmower@gmail.com>
2024-04-18 11:41:08 +0100

    enemie moving from one point to another

---
+++ Assets/Scripts/Kian Scripts/ChangeTime.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ChangeTime : MonoBehaviour
+{
+    public GameObject present;
+    public GameObject future;    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            future.SetActive(true);
+            present.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            future.SetActive(false);
+            present.SetActive(true);
+        }
+    }    
+}
