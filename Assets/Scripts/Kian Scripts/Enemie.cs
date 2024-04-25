e2ab380f3fd0a0e94429e74b56a7f2bc28f4ad59
NebulaPhoenix1 <xaviermdowney@gmail.com>
2024-04-25 10:34:00 +0100

    10:21

--- Assets/Scripts/Kian Scripts/Enemie.cs
+++ Assets/Scripts/Kian Scripts/Enemie.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Enemie : MonoBehaviour
 {
     float speed = 5f;
+    float bulletSpeed = 1000f;
 
-    bool toPointOne = true;
-    bool toPointTwo = false;
+    bool toNextPoint = true;
+    bool canFire = true;
 
     Transform target1;
     Transform target2;
 
+    public GameObject bullet;
+    public GameObject bulletFirePoint;
     public GameObject moveTo;
     public GameObject moveTo2;
-
+    public GameObject player;
 
     private void Awake()
     {
@@ -26,30 +29,92 @@ public class Enemie : MonoBehaviour
     {
         var moving = speed * Time.deltaTime;
 
-        if (toPointOne == true)
+        if (toNextPoint)
         {
             transform.position = Vector3.MoveTowards(transform.position, target1.position, moving);
-            transform.localScale = new Vector3(1f, 0.5f, 1f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(-Vector2.right) * 5f, Color.red);
+
+            RaycastHit2D hit = Physics2D.Raycast(bulletFirePoint.transform.position, bulletFirePoint.transform.TransformDirection(-Vector2.right), 5f);
+
+            if (hit)
+            {
+                speed = 0;
+                if (canFire && hit.collider.name == "Player")
+                {
+                    StartCoroutine(fire());
+                }
+            }
+            else
+            {
+                speed = 5;
+            }
 
             if (Vector3.Distance(transform.position, target1.position) < 0.001f)
             {
-                toPointTwo = true;
-                toPointOne = false;
+                toNextPoint = false;
             }
         }
 
-        if (toPointTwo == true)
+        if (!toNextPoint)
         {
             transform.position = Vector3.MoveTowards(transform.position, target2.position, moving);
-            transform.localScale = new Vector3(-1f, 0.5f, 1f);
+            transform.localScale = new Vector3(-1f, 1, 1f);
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * 5f, Color.red);
+
+            RaycastHit2D hit = Physics2D.Raycast(-bulletFirePoint.transform.position, -bulletFirePoint.transform.TransformDirection(Vector2.right), 5f);
+
+            if (hit)
+            {
+                speed = 0;
+                if (canFire && hit.collider.name == "Player")
+                {
+                    StartCoroutine(fire());
+                }
+            }
+            else
+            {
+                speed = 5;
+            }
 
             if (Vector3.Distance(transform.position, target2.position) < 0.001f)
             {
-                toPointOne = true;
-                toPointTwo = false;
+                toNextPoint = true;
             }
-
         }
 
+    }
+
+    IEnumerator fire()
+    {
+        if (toNextPoint)
+        {
+            GameObject cloneBullet = Instantiate(bullet, bulletFirePoint.transform.position, transform.rotation);
+            Rigidbody2D cloneBulletRb = cloneBullet.GetComponent<Rigidbody2D>();
+
+            cloneBulletRb.AddForce(-bulletFirePoint.transform.right * bulletSpeed);
+
+            canFire = false;
+
+            yield return new WaitForSeconds(2f);
+
+            canFire = true;
+        }
+
+        if (!toNextPoint)
+        {
+            GameObject cloneBullet = Instantiate(bullet, bulletFirePoint.transform.position, transform.rotation);
+            Rigidbody2D cloneBulletRb = cloneBullet.GetComponent<Rigidbody2D>();
+
+            cloneBulletRb.AddForce(bulletFirePoint.transform.right * bulletSpeed);
+
+            canFire = false;
+
+            yield return new WaitForSeconds(2f);
+
+            canFire = true;
+        }
     }
 }
