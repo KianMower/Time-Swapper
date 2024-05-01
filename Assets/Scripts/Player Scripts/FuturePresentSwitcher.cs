5bd23904ec5318facb228125af0bcb3c2b065fdc
TheIronDan <161759661+IronDan20@users.noreply.github.com>
2024-05-01 16:34:58 +0100

    Scenes upgraded with the new time swapping mechanic courtesy of Luke

--- Assets/Scripts/Player Scripts/FuturePresentSwitcher.cs
+++ Assets/Scripts/Player Scripts/FuturePresentSwitcher.cs
@@ -5,25 +5,57 @@ using UnityEngine;
 public class FuturePresentSwitcher : MonoBehaviour
 {
     bool inFuture = false;
+    [Header("Toggling Gameobjects Method")]
     [SerializeField] GameObject present;
     [SerializeField] GameObject future;
+
+    //teleporting test//
+    [Header("Teleporting method")]
+    public bool useTeleportMethod = false;
+    [SerializeField] GameObject playerCam;
+
 
     // Update is called once per frame
     void Update()
     {
-        //Teleport to present (left click)
-        if(Input.GetMouseButtonDown(0) && (inFuture))
+        //Teleporting player and camera method
+        if (useTeleportMethod)
         {
-            future.SetActive(false);
-            present.SetActive(true);
-            inFuture = !inFuture;
+            //Teleport to present (left click)
+            if (Input.GetMouseButtonDown(0) && (inFuture))
+            {
+                inFuture = !inFuture;
+                transform.position = new Vector3(transform.position.x, transform.position.y - 600, transform.position.z);
+                playerCam.transform.position = new Vector3(playerCam.transform.position.x, playerCam.transform.position.y - 600, playerCam.transform.position.z);
+            }
+            //Teleport to future (right click)
+            if (Input.GetMouseButtonDown(1) && (!inFuture))
+            {
+                inFuture = !inFuture;
+                transform.position = new Vector3(playerCam.transform.position.x, playerCam.transform.position.y + 600, playerCam.transform.position.z);
+                playerCam.transform.position = new Vector3(playerCam.transform.position.x, playerCam.transform.position.y + 600, playerCam.transform.position.z);
+            }
         }
-        //Teleport to future (right click)
-        if(Input.GetMouseButtonDown(1) && (!inFuture))
+        //Old (laggy) method
+        else
         {
-            future.SetActive(true);
-            present.SetActive(false);
-            inFuture = !inFuture;
+            //Teleport to present (left click)
+            if (Input.GetMouseButtonDown(0) && (inFuture))
+            {
+                future.SetActive(false);
+                present.SetActive(true);
+                inFuture = !inFuture;
+            }
+            //Teleport to future (right click)
+            if (Input.GetMouseButtonDown(1) && (!inFuture))
+            {
+                future.SetActive(true);
+                present.SetActive(false);
+                inFuture = !inFuture;
+            }
         }
     }
+
+
 }
+
