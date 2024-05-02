71017d762b2ba84002b4f0b20298c912bcea096a
KianMower <kianmower@gmail.com>
2024-05-02 12:17:34 +0100

    12:17 02/05/24

--- Assets/Scripts/Player Scripts/FuturePresentSwitcher.cs
+++ Assets/Scripts/Player Scripts/FuturePresentSwitcher.cs
@@ -8,6 +8,8 @@ public class FuturePresentSwitcher : MonoBehaviour
     [Header("Toggling Gameobjects Method")]
     [SerializeField] GameObject present;
     [SerializeField] GameObject future;
+    [SerializeField] GameObject presentCogs;
+    [SerializeField] GameObject futureCogs;
     
 
     //teleporting test//
@@ -28,6 +30,7 @@ public class FuturePresentSwitcher : MonoBehaviour
                 inFuture = !inFuture;
                 transform.position = new Vector3(transform.position.x, transform.position.y - 300, transform.position.z);
                 playerCam.transform.position = new Vector3(playerCam.transform.position.x, playerCam.transform.position.y - 300, playerCam.transform.position.z);
+                
             }
             //Teleport to future (right click)
             if (Input.GetMouseButtonDown(1) && (!inFuture))
@@ -35,6 +38,7 @@ public class FuturePresentSwitcher : MonoBehaviour
                 inFuture = !inFuture;
                 transform.position = new Vector3(transform.position.x, transform.position.y + 300, transform.position.z);
                 playerCam.transform.position = new Vector3(playerCam.transform.position.x, playerCam.transform.position.y + 300, playerCam.transform.position.z);
+               
             }
         }
         //Old (laggy) method
@@ -43,15 +47,17 @@ public class FuturePresentSwitcher : MonoBehaviour
             //Teleport to present (left click)
             if (Input.GetMouseButtonDown(0) && (inFuture))
             {
-                future.SetActive(false);
-                present.SetActive(true);
+               
+                presentCogs.SetActive(true);
+                futureCogs.SetActive(false);
                 inFuture = !inFuture;
             }
             //Teleport to future (right click)
             if (Input.GetMouseButtonDown(1) && (!inFuture))
             {
-                future.SetActive(true);
-                present.SetActive(false);
+                
+                presentCogs.SetActive(false);
+                futureCogs.SetActive(true);
                 inFuture = !inFuture;
             }
         }
