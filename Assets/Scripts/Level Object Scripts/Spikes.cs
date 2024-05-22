f4082c96cb3b8109678c6c9419b05672517c33df
NebulaPhoenix1 <luke.robb210@gmail.com>
2024-05-22 10:57:36 +0100

    Fixed jump and tweaked spikes
    - Moved ground check to back leg of player which allows for jumping when on the edge of a tile
    - Changed serialize field for health controller in Spikes.cs to private + get component making adding spikes easier
    - Spikes reactivate instantly after player leaves trigger zone

--- Assets/Scripts/Level Object Scripts/Spikes.cs
+++ Assets/Scripts/Level Object Scripts/Spikes.cs
@@ -7,7 +7,7 @@ public class Spikes : MonoBehaviour
     private bool spikesActive = true;
     public float gracePeriod;
     public float damageAmount;
-    [SerializeField] HealthController healthController;
+    private HealthController healthController;
 
     //If player collides with a spike and its active, run spikesTrigger
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,7 +15,18 @@ public class Spikes : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") && spikesActive)
         {
             //Debug.Log("spikes hit player");
+            healthController = collision.gameObject.GetComponent<HealthController>();
             StartCoroutine(spikesTrigger());
+        }
+    }
+
+    //If player gets off spike, reactivate spike
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            StopCoroutine(spikesTrigger());
+            spikesActive = true;
         }
     }
 
