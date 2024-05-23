f6126da127c8ed411c611916c59b9d64332a8ad5
Luke <luke.robb210@gmail.com>
2024-05-23 12:10:29 +0100

    Goals Updated
    Take you instantly to next level instead of to a temporary menu

--- Assets/Dan's Scripts/TemporarySceneNavigator.cs
+++ Assets/Dan's Scripts/TemporarySceneNavigator.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class TemporarySceneNavigator : MonoBehaviour
 {
+
+    //O = tutorial, 1 = lv1, 2=lv2
+    public float sceneToLoad;
+
     public void BeginTutorial()
     {
         SceneManager.LoadScene("Tutorial Scene");
@@ -29,7 +35,24 @@ public class TemporarySceneNavigator : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            SceneManager.LoadScene("TemporaryMenu");
+            if(sceneToLoad == 0)
+            {
+                SceneManager.LoadScene("Tutorial Scene");
+            }
+            if (sceneToLoad == 1)
+            {
+                SceneManager.LoadScene("Level 1 Scene");
+            }
+            if (sceneToLoad == 2)
+            {
+                SceneManager.LoadScene("Level 2 Scene");
+            }
+            if (sceneToLoad == 3)
+            {
+                SceneManager.LoadScene("Level 3 Scene");
+            }
+
+
         }
     }
 }
