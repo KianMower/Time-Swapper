829a4bec2879f8798b7fce0cbdcbd4d5246966e6
IronDan20 <161759661+IronDan20@users.noreply.github.com>
2024-05-23 10:23:48 +0100

    Fixed doors. door sprites and some sounds

--- Assets/Scripts/Level Object Scripts/doorScript.cs
+++ Assets/Scripts/Level Object Scripts/doorScript.cs
@@ -10,23 +10,40 @@ public class doorScript : MonoBehaviour
      * sets open to true, and the twp halves of the door have their Y coordinate changed over time. When the player leaves the collider, 
      * the opposite happens and the Y coordinate is chased back to its original value. */
 
-
     [SerializeField] GameObject topHalf;
     [SerializeField] GameObject bottomHalf;
-    Vector3 topHalfInitPos;
-    Vector3 bottomHalfInitPos;
+    Vector2 topHalfInitPos;
+    Vector2 bottomHalfInitPos;
+
+    private Vector2 topHalfInitStartPos;
+    private Vector2 bottomHalfInitStartPos;
+    private Vector2 topHalfInitendPos;
+    private Vector2 bottomHalfInitendPos;
+
     public float openHeight;
 
 
     float timer = 0;
     public float openTime = 1f;
     private bool opening = false;
-    private bool closed = true;
+    
 
     private void Start()
     {
         topHalfInitPos = topHalf.transform.position;
         bottomHalfInitPos = bottomHalf.transform.position;
+
+        topHalfInitStartPos = topHalfInitPos;
+        bottomHalfInitStartPos = bottomHalfInitPos;
+
+        topHalfInitendPos = new Vector2(topHalfInitPos.x, topHalfInitPos.y + openHeight);
+        bottomHalfInitendPos = new Vector2(topHalfInitPos.x, topHalfInitPos.y - openHeight);
+    }
+
+    private void Update()
+    {
+        topHalfInitendPos = new Vector2(topHalfInitPos.x, topHalfInitPos.y + openHeight);
+        bottomHalfInitendPos = new Vector2(topHalfInitPos.x, topHalfInitPos.y - openHeight);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -35,7 +52,9 @@ public class doorScript : MonoBehaviour
         {
             //Debug.Log("enter");
             opening = true;
-            closed = false;
+            timeElapsed = 0;
+
+
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -44,28 +63,52 @@ public class doorScript : MonoBehaviour
         {
             //Debug.Log("exit");
             opening = false;
-            closed = true;
+
+            timeElapsed = 0;
+
+
         }
     }
 
+
+
+    private float timeElapsed=0;
+    public float lerpDuration = 3;
     private void FixedUpdate()
     {
-        if (timer <= openTime && opening)
-        {
-            timer += Time.deltaTime;
-            float result = (timer / openTime);
-            //Debug.Log(result);
-            topHalf.transform.position = new Vector3(topHalfInitPos.x, topHalfInitPos.y + (openHeight * result), topHalfInitPos.z);
-            bottomHalf.transform.position = new Vector3(bottomHalfInitPos.x, bottomHalfInitPos.y - (openHeight * result), bottomHalfInitPos.z);
-        }
+        //if (timer <= openTime && opening)
+        //{
+        //    timer += Time.deltaTime;
+        //    float result = (timer / openTime);
+        //    Debug.Log("timer <= openTime && opening");
+        //    topHalf.transform.position = new Vector2(topHalfInitPos.x, topHalfInitPos.y + (openHeight * result));
+        //    bottomHalf.transform.position = new Vector2(bottomHalfInitPos.x, bottomHalfInitPos.y - (openHeight * result));
+        //}
 
-        if (timer >= 0f && closed)
+        //if (timer >= 0f && !opening)
+        //{
+        //    timer -= Time.deltaTime;
+        //    float result = (timer / openTime);
+        //    Debug.Log("result " +result);
+        //    topHalf.transform.position = new Vector2(topHalfInitPos.x, topHalfInitPos.y + (openHeight * result));
+        //    bottomHalf.transform.position = new Vector2(bottomHalfInitPos.x, bottomHalfInitPos.y - (openHeight * result));
+        //}
+        //Debug.Break();
+
+        if(opening &&  timeElapsed < lerpDuration)
         {
-            timer -= Time.deltaTime;
-            float result = (timer / openTime);
-            //Debug.Log(result);
-            topHalf.transform.position = new Vector3(topHalfInitPos.x, topHalfInitPos.y + (openHeight * result), topHalfInitPos.z);
-            bottomHalf.transform.position = new Vector3(bottomHalfInitPos.x, bottomHalfInitPos.y - (openHeight * result), bottomHalfInitPos.z);
+
+            float lerpTime = timeElapsed / lerpDuration;
+            timeElapsed += Time.deltaTime;
+            topHalf.transform.position = Vector3.Lerp(topHalfInitStartPos, topHalfInitendPos, lerpTime);
+            bottomHalf.transform.position = Vector3.Lerp(bottomHalfInitStartPos, bottomHalfInitendPos, lerpTime);
+        }
+        else if (!opening && timeElapsed < lerpDuration)
+        {
+            float lerpTime = timeElapsed / lerpDuration;
+            timeElapsed += Time.deltaTime;
+            topHalf.transform.position = Vector3.Lerp(topHalfInitendPos, topHalfInitStartPos, lerpTime);
+            bottomHalf.transform.position = Vector3.Lerp(bottomHalfInitendPos, bottomHalfInitStartPos, lerpTime);
         }
 
     }
