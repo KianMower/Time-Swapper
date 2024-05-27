da3c49ea211aa1fa17de30c19c3048e1454f0631
TheIronDan <161759661+IronDan20@users.noreply.github.com>
2024-05-27 10:58:17 +0100

    Update TemporaryDeath.cs

--- Assets/Scripts/Player Scripts/TemporaryDeath.cs
+++ Assets/Scripts/Player Scripts/TemporaryDeath.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class TemporaryDeath : MonoBehaviour
 {
-    [SerializeField] private HealthController playerHealth;
-    public ParticleSystem electricity;
-    public AudioSource damageSFX;
+    [SerializeField] private HealthController playerHealth; // Creates a link to the programmer's player health script
+    public ParticleSystem electricity; // The damage VFX
+    public AudioSource damageSFX; // The damage SFX
 
     // Start is called before the first frame update
     void Start()
@@ -18,21 +18,21 @@ public class TemporaryDeath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(playerHealth.health <= 0)
+        if(playerHealth.health <= 0) // Calls the death function when the health is 0 or less than 0
         {
-            death();
+            death(); 
         }
     }
 
     public void death()
     {
         Debug.Log("death ran");
-        SceneManager.LoadScene("TemporaryMenu");
+        SceneManager.LoadScene("TemporaryMenu"); // Resets to the level select when death happens
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Enemy")
+        if(collision.tag == "Enemy") //On collision with an enemy this plays the VFX and SFX for damaging while subtracting health
         {
             electricity.Play();
             damageSFX.Play();
